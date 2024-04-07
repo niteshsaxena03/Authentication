74e42fba7d19751bd535bc1448cd69426faf9f99
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-07 07:57:09 +0530

    Creating 2 functions for creating user and login

--- util/auth.js
+++ util/auth.js
@@ -1,15 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-{/*Firebase rest api is being used*/}
-const API_KEY = 'AIzaSyBXagRCAsqcJNoQ1dUqjPdE1GNoeubxlE4';
+{
+  /*Firebase rest api is being used*/
+}
+const API_KEY = "AIzaSyBXagRCAsqcJNoQ1dUqjPdE1GNoeubxlE4";
 
-export async function createUser(email,password){
-    const response=await axios.post(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY,
-      {
-        email:email,
-        password:password,
-        returnSecureToken:true
-      }
-    );
+async function authenticate(mode, email, password) {
+  const url =
+    `https://identitytoolkit.googleapis.com/v1/account:${mode}?key=` + API_KEY;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
+export async function createUser(email, password) {
+ await authenticate("signUp", email, password);
+}
+
+export async function createUser(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
