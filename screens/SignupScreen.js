a270e84055fb1ecb054fb3853e51ca35dd9dca4d
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-08 09:50:24 +0530

    Using the functions of signup and login in different files to login the user after signing up

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,22 +1,23 @@
-import AuthContent from '../components/Auth/AuthContent';
-import { createUser } from '../util/auth';
-import {useState} from "react";
-import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { useState } from "react";
+
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../util/auth";
 
 function SignupScreen() {
-  const [isAuthenticating,setIsAuthenticating]=useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  async function signupHandler({email,password}){
+  async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email,password);
+    await createUser(email, password);
     setIsAuthenticating(false);
   }
 
-  if(isAuthenticating){
-    return <LoadingOverlay message="Creating user..."/>
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
   }
+
   return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
- 
