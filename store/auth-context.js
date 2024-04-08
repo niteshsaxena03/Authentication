543d43fc0d60ba348ccd497224a31fddb1dfad82
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-08 10:29:44 +0530

    Creating a store using context api in order to do state management in authentication

---
+++ store/auth-context.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: () => {},
+  logOut: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState("");
+
+  function authenticate(token) {
+    setAuthToken(token);
+  }
+
+  function logOut() {
+    setAuthToken(null);
+  }
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logOut: logOut,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+export default AuthContextProvider;
